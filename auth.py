fa28d1f128feb44ba0a8b8a10d563d1bcc879dca
heraliepniece <hera.liepniece@gmail.com>
2025-05-10 17:27:29 +0100

    fixing issues, adding session,  fixing password issues

--- auth.py
+++ auth.py
@@ -1,10 +1,11 @@
 import flask
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session
 import pyotp
 import csv
 import secrets
 from flask_mail import Mail, Message
 from flask_bcrypt import Bcrypt
+
 
 # Database shall go here
 
@@ -12,24 +13,27 @@ from flask_bcrypt import Bcrypt
 # 
 #Flask Set Up for Auth
 app = Flask(__name__)
-
+app.secret_key = '51275a136660d5b822051b9d4180bf51848855b61b6e79a2262c89d1998dc459'
 bcrypt = Bcrypt(app)
 
 
 #Setting up email 
-flask-mail.MAIL_SERVER = 'smtp.gmail.com'
-flask-mail.MAIL_PORT = 587
-flask-mail.MAIL_USE_TLS = True
-flask-mail.MAIL_USE_SSL = False
-flask-mail.MAIL_USERNAME = 'hera.liepniece@gmail.com'
-flask-mail.MAIL_PASSWORD = 'jlch tsib qadb nf'
-flask-mail.MAIL_DEFAULT_SENDER: 'hera.liepniece@gmail.com'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USERNAME'] = 'hera.liepniece@gmail.com'
+app.config['MAIL_PASSWORD'] = 'jlch tsib qadb nf'
+app.config['MAIL_DEFAULT_SENDER'] = 'hera.liepniece@gmail.com'
 
 mail = Mail(app)
 
 def generate_OTP():
             otp = secrets.randbelow(900000) 
+            session['otp'] = str(otp)
             return otp
+
+            
 
 def otp_email(user_email, otp):
        msg = Message("One-Time Password for Registration", recipients=[user_email])
@@ -63,14 +67,15 @@ def check_otp():
 
 
 #new page that allows users to set up a new password:
-@app.route('/new_password')
+@app.route('/new_password', methods=[ 'GET', 'POST'])
 def create_password():
-         new_password = request.form.get('new_password')
+         if request.method == 'POST':
+            new_password = request.form.get('new_password')
 
-         hashed_password = bcrypt.generate_password_hash('password').decode('utf-8')
+            hashed_password = bcrypt.generate_password_hash(new_password).decode('utf-8')
 
-         is_valid = bcrypt.check_password_hash(hashed_password, 'password')
-         
+            return 'Your password has been set!'
+
 # POST requests for login- checks for OTP instead of a password and redirects user to change it
 @app.route('/login', methods=['POST'])
 def login():
