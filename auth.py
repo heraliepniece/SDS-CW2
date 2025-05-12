eb94e2c43e98557ff345caa28478c84c6587091e
elyanazafar <elyanazafar@gmail.com>
2025-05-12 18:49:21 +0100

    Update main.py

--- auth.py
+++ auth.py
@@ -1,120 +1,121 @@
-import flask
-from flask import Flask, request, render_template, session
-import pyotp
-import csv
-import secrets
+from flask import render_template, request, redirect, url_for, session
 from flask_mail import Mail, Message
 from flask_bcrypt import Bcrypt
+import secrets
+from tables import User
 
 
-# Database shall go here
-
-
-# 
 #Flask Set Up for Auth
-app = Flask(__name__)
-app.secret_key = '51275a136660d5b822051b9d4180bf51848855b61b6e79a2262c89d1998dc459'
-bcrypt = Bcrypt(app)
-
+bcrypt = Bcrypt()
+mail = Mail()
 
 #Setting up email 
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
-app.config['MAIL_USERNAME'] = 'hera.liepniece@gmail.com'
-app.config['MAIL_PASSWORD'] = 'jlch tsib qadb nf'
-app.config['MAIL_DEFAULT_SENDER'] = 'hera.liepniece@gmail.com'
+def register_routes(app, db_session):
+      app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+      app.config['MAIL_PORT'] = 587
+      app.config['MAIL_USE_TLS'] = True
+      app.config['MAIL_USE_SSL'] = False
+      app.config['MAIL_USERNAME'] = 'hera.liepniece@gmail.com'
+      app.config['MAIL_PASSWORD'] = 'jlch tsib qadb nf'
+      app.config['MAIL_DEFAULT_SENDER'] = 'hera.liepniece@gmail.com'
+      mail.init_app(app)
+      bcrypt.init_app(app)
 
-mail = Mail(app)
 
-def generate_OTP():
+      def generate_OTP():
             otp = secrets.randbelow(900000) 
             session['otp'] = str(otp)
             return otp
 
+      
+      def otp_email(user_email, otp):
+            msg = Message("One-Time Password for Registration", recipients=[user_email])
+            msg.body =f"Your one-time password is: {otp}\n This password will expire in 10 minutes."
+            mail.send(msg)
+
+      #User Registration with OTP
+      @app.route('/registration', methods=['GET', 'POST'])
+      def registration():
+            if request.method == 'POST':
+                  user_email= request.form.get('email')
+                  session['email'] = user_email
+                  otp = generate_OTP()
+                  otp_email(user_email,otp)
+                  return redirect(url_for('check_otp'))
+            return render_template('register.html')   
+
+      @app.route('/otp_check', methods=['GET', 'POST'])
+      def check_otp():
+            if request.method == 'POST':
+                  user_otp = request.form['otp']
+                  if user_otp == session.get('otp'):
+                        return redirect(url_for('create_password'))
+                  return "Invalid OTP. Try again."
+            return render_template('otp_check.html')
+
+      @app.route('/role_select', methods=['GET', 'POST'])
+      def role_select():
+            if request.method == 'POST':
+                  role_selection = request.form.get('role_select')
+                  session['role_select'] = role_selection
+                  if role == 'team_member':
+                        return redirect(url_for('tm_login'))
+                  elif role == 'project_manager':
+                        return redirect(url_for('pm_login'))
+            return render_template('role_select.html')
+
+      #Home route for login form
+      @app.route('/')
+      def home():
+            return render_template('login.html')
+
+      #check if the otp entered is valid
+      @app.route('/otp_check', methods=['GET', 'POST'])
+      def check_otp():
+            user_otp = request.form.get('otp')
+
+            if user_otp == session.get('otp'):
+                  return redirect(url_for('new_password'))
+            else:
+                  return 'Incorrect OTP. Please Try Again.'
+
+
+      #new page that allows users to set up a new password:
+      @app.route('/new_password', methods=[ 'GET', 'POST'])
+      def create_password():
+            if request.method == 'POST':
+                  new_password = request.form.get('new_password')
+                  email = session.get('email')
+                  hashed_password = bcrypt.generate_password_hash(new_password).decode('utf-8')
+
+                  return 'Your password has been set!'
+
+      # POST requests for login- checks for OTP instead of a password and redirects user to change it
+      @app.route('/tm_login', methods=['POST'])
+      def tm_login():
+            if request.method == 'POST':
+                  username = request.form.get('username')
+                  password = request.form.get('password')
+
+                  email = request.form.get('username')
+                  valid_password = request.form.get('password')
             
+                  if username == email and password == valid_password:
+                        return f'Welcome! Login succesful.'
+                  else:
+                        return 'Invalid username or password.Please try again.'
 
-def otp_email(user_email, otp):
-       msg = Message("One-Time Password for Registration", recipients=[user_email])
-       msg.body =f"Your one-time password is: {otp}\n This password will expire in 10 minutes."
-       mail.send(msg)
+      @app.route('/pm_login', methods=['POST'])
+      def pm_login():
+            if request.method == 'POST':
+                  username = request.form.get('username')
+                  password = request.form.get('password')
 
-#User Registration with OTP
-@app.route('/registration', methods=['GET', 'POST'])
-def registration():
-    if request.method == 'POST':
-        user_email= request.form.get('email')
-        session['email'] = user_email
-        otp = generate_OTP()
-        otp_email(user_email,otp)
-    return render_template('register.html')   
-
-@app.route('/role_select', methods=['GET', 'POST'])
-def role_select():
-      if request.method == 'POST':
-            role_selection = request.form.get('role_select')
-            session['role_select'] = role_selection
-            if role == 'team_member':
-                  return redirect(url_for('tm_login'))
-            elif role == 'project_manager':
-                  return redirect(url_for('pm_login'))
-      return render_template('role_select.html')
-
-#Home route for login form
-@app.route('/')
-def home():
-     return render_template('login.html')
-
-#check if the otp entered is valid
-@app.route('/otp_check', methods=['GET', 'POST'])
-def check_otp():
-      user_otp = request.form.get('otp')
-
-      if user_otp == session.get('otp'):
-            return redirect(url_for('new_password'))
-      else:
-            return 'Incorrect OTP. Please Try Again.'
+                  if username == projectmanager and password == eyespy:
+                        return f'Welcome Project Manager! Login succesful.'
+                  else:
+                        return 'Invalid username or password. Please try again.'
 
 
-#new page that allows users to set up a new password:
-@app.route('/new_password', methods=[ 'GET', 'POST'])
-def create_password():
-         if request.method == 'POST':
-            new_password = request.form.get('new_password')
-            email = session.get('email')
-            hashed_password = bcrypt.generate_password_hash(new_password).decode('utf-8')
-
-            return 'Your password has been set!'
-
-# POST requests for login- checks for OTP instead of a password and redirects user to change it
-@app.route('/tm_login', methods=['POST'])
-def tm_login():
-        if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-
-        email = request.form.get('username')
-        valid_password = request.form.get('password')
-        
-        if username == email and password == valid_password:
-            return f'Welcome! Login succesful.'
-        else:
-                return 'Invalid username or password.Please try again.'
-
-@app.route('/pm_login', methods=['POST'])
-def pm_login():
-        if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-
-        if username == projectmanager and password == eyespy:
-            return f'Welcome Project Manager! Login succesful.'
-        else:
-                return 'Invalid username or password. Please try again.'
-
-
-if __name__=='__main__':
-     app.run(debug=True)
 
 
--- main.py
+++ main.py
@@ -1,22 +1,24 @@
-import datetime
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
+from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from tables import Base
 
 
-Base = declarative_base()
-engine = create_engine("sqlite:///app.db")  #Uses SQLite to store the database in a file called app.db
-
-class User(Base):
-    __tablename__ = 'users' #Creates a table called users
-
-    id = Column(Integer, primary_key=True)
-    username = Column(String(50), unique=True, nullable=False)
-    password = Column(String(200), nullable=False)
-    role = Column(String(20), nullable=False)  #'team_member' or 'project_manager'
+app = Flask(__name__) #Figure out name
+app.secret_key = '51275a136660d5b822051b9d4180bf51848855b61b6e79a2262c89d1998dc459'
 
 
+#SQLite setup
+engine = create_engine("sqlite:///app.db")
 Base.metadata.create_all(engine)
-
 Session = sessionmaker(bind=engine)
-session = Session()
+db_session = Session()
+
+#Register routes
+from auth import register_routes
+register_routes(app, db_session)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+ 
+
